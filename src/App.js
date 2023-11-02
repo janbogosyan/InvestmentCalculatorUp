2d6183b4991d03c0bda9aef1163a365ddb81335e
Jan Bogosyan <janbogosyan@gmail.com>
2023-11-02 16:06:14 +0200

    Outputing results in a list and deriving more values

--- src/App.js
+++ src/App.js
@@ -18,10 +18,15 @@ function handleChange(inputIdentifier, newValue) {
     setUserInput(prevUserInput => {
         return {
             ...prevUserInput,
-            [inputIdentifier]: +newValue      //обръщаме newValue да е число
-        };
+            [inputIdentifier]: +newValue  // The key specified by inputIdentifier is updated in the new state object. обръщаме newValue да е число
+        };                                // Finally, the new state object is returned, and it will be used by React to update the component's state,
+                                          // re-render the component, and reflect the changes in the UI.
     });
 };
+//In summary, this function is designed to handle changes in user input fields and update the component's state accordingly,
+//ensuring that the updated state contains the new value for the specified input field.
+//This is a common pattern in React for handling form inputs.
+
 
   return (
     <>
